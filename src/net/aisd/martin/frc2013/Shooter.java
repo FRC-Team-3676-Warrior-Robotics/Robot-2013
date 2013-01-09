ac2b14592ac87e9448a16617c4cabd88dfd3af74
Neil Manning <nwmanning21@gmail.com>
2013-01-08 21:25:44 -0600

    Added ImageProcessing, Shooter, Climber
    Added ImageProcessing. Will tell you which target and the range, and
    position. WIll eventually talk to shooter and drivetrain to allow for
    firing solutions to be made automatically.
    Added Shooter place holder.
    Added Climber place holder.

---
+++ src/net/aisd/martin/frc2013/Shooter.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.aisd.martin.frc2013;
+
+/**
+ * Empty Class till we know what to do with it
+ * @author Neil
+ */
+public class Shooter {
+	
+}
